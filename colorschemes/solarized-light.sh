afcdc1dc7adf868d2b3e4178663556fc8c09b514
Rovanion Luckey <rovanion.luckey@gmail.com>
2021-03-19 11:31:50 +0100

    Remove borders from all color schemes

--- colorschemes/solarized-light.sh
+++ colorschemes/solarized-light.sh
@@ -3,7 +3,7 @@
 # See the Solarized homepage for details:
 # http://ethanschoonover.com/solarized
 #
-                   # SOLARIZED HEX     16/8 TERMCOL   L*A*B     
+                   # SOLARIZED HEX     16/8 TERMCOL   L*A*B
                    # --------- ------- ---- --------- ----------
  base03="#002b36"  # base03    #002b36  8/4 brblack   15 -12 -12
  base02="#073642"  # base02    #073642  0/4 black     20 -12 -12
@@ -28,7 +28,6 @@ cursor="$base01"
 mouse_background="$base2"
 mouse_foreground="$base01"
 highlight="$base2"
-border="$base3"
 
  color0="$base2"   # black
  color8="$base3"   # brblack
